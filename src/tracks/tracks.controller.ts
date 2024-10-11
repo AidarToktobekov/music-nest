41ffbe23d9bc3615439af47f2b33125880d1179e
Toktobekov Aidar <aidar.omurbekov05@gmail.com>
2024-10-11 17:16:59 +0600

    Добавил роут удаления треков

--- src/tracks/tracks.controller.ts
+++ src/tracks/tracks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Track, TrackDocument } from 'src/schemas/track.schema';
@@ -25,8 +25,6 @@ export class TracksController {
         @Body() trackDto: CreateTracktDto 
     ){
 
-        const allTracks = this.trackModel.find({album: trackDto.album});
-        
         const track = new this.trackModel({
             name: trackDto.name,
             album: trackDto.album,
@@ -36,5 +34,10 @@ export class TracksController {
         })
 
         return track.save();
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string){
+        return this.trackModel.findByIdAndDelete({_id: id});
     }
 }
